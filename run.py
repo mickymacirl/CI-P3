8438a6ddf82e9bf562736c1a75d4f1c3e641249b
mickymacirl <mickymacster@gmail.com>
2022-08-16 17:52:39 +0000

    add stock data from our spreadsheet

--- run.py
+++ run.py
@@ -57,15 +57,14 @@ def validate_data(values):
     return True
 
 
-def update_worksheet(data, worksheet):
+def update_sales_worksheet(data):
     """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
+    Update sales worksheet, add new row with the list data provided
     """
-    print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet(worksheet)
-    worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully\n")
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -78,12 +77,7 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    surplus_data = []
-    for stock, sales in zip(stock_row, sales_row):
-        surplus = int(stock) - sales
-        surplus_data.append(surplus)
-
-    return surplus_data
+    print(stock_row)
 
 
 def main():
@@ -92,9 +86,8 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_worksheet(sales_data, "sales")
-    new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data, "surplus")
+    update_sales_worksheet(sales_data)
+
 
 print(pyfiglet.figlet_format("Test Ascii",font='slant',justify="center"))
 print("######################################################################")
